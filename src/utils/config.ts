bb8cfbc958ec9b3c99db6e49b5043d225f13dbc1
loveyunk <mjjccc@126.com>
2019-03-27 19:17:30 +0800

    Adding config file

---
+++ src/utils/config.ts
@@ -0,0 +1,6 @@
+export const BASE_URL_DEV: string = ''; // 开发环境 baseURL
+export const BASE_URL_PRO: string = ''; // 生产环境 baseURL
+
+export const TIME_OUT: number = 60000; // 请求超时时间
+
+export const CANCEL_REQUEST_MESSAGE = 'cancel request';
